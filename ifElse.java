2ca72f8f3fa5b1c09273dd5544d2f9388d2db48b
Sophia <sophianguyen22072024@gmail.com>
2024-09-10 22:52:33 +0700

    Updated 1009

---
+++ ifElse.java
@@ -0,0 +1,13 @@
+public class ifElse {
+
+    public static void main(String[] args) {
+        int a = 10;
+
+        if(a % 2 == 0) {
+            System.out.println("So chan");
+        } else {
+            System.out.println("So le");
+       }
+    }
+}
+
